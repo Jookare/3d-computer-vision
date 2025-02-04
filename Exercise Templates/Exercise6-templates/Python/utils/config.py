37b4430e8f0a3ccd6482b842725b54684ce5d57f
jookare <joonakareinen@gmail.com>
2025-02-04 14:55:05 +0200

    Add exercise 6 task 2 files

--- Exercise Templates/Exercise6-templates/Python/utils/config.py
+++
@@ -1,5 +0,0 @@
-EPS = 1e-6
-
-GRID_SIZE   = 7   # Divide each image into a SxS grid   S
-N_BBOX      = 1   # Number of bounding boxes to predict B 
-N_CLASSES   = 1   # Number of classes in the dataset    C

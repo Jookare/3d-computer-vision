770baff5b0e61796cba0440821eab61dca6e8d38
jookare <joonakareinen@gmail.com>
2025-03-10 13:56:11 +0200

    Add exercise 8 templates

---
+++ Exercise Templates/Exercise8-templates/Matlab/template.m
@@ -0,0 +1,52 @@
+% Computer Vision - Exercise 8
+% FIRSTNAME LASTNAME
+
+%% Task 1
+clc
+close all
+clearvars
+
+% TODO: Find points from images 
+
+% One option is to use ginput
+points = ginput(9)
+
+
+% TODO: expand and normalize points
+function [T, np] = norm_points(p)
+
+end
+
+
+% TODO: Build matrix A
+
+
+% TODO: Compute F from A
+
+
+% TODO: Compute F_dash from F. Make sure you know what np.linalg.svd returns
+
+
+% TODO Denormalize and test avlue using ninth point
+
+
+%% Task 2
+clc
+close all
+clearvars
+
+% TODO Use F to find left and right epipoles
+
+% TODO Plot the ninth point to the left image and right epipolar line to right
+% Find the line coefficients from F @ el and plot the line
+
+tiledlayout(1,2)
+nexttile()
+imshow(img_l)
+hold on
+title("Left image")
+
+nexttile()
+imshow(img_r)
+hold on
+title("Right image")
